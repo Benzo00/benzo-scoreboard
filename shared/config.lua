1dba92891c5fb8c9c66350ef45e8f577c37cd3d9
Benzo <102178921+Benzo00@users.noreply.github.com>
2022-09-12 03:56:49 +0200

    brrrrrrrrrrrrrrrrrrrrr

---
+++ shared/config.lua
@@ -0,0 +1,12 @@
+Config = {}
+
+Config.Key = "delete"
+
+Config.ShowJob = true
+Config.ShowJobGrade = true 
+Config.ShowGroup = true
+Config.ShowPing = true
+Config.ShowID = true
+Config.ShowName = true
+Config.ShowHealth = true
+Config.ShowArmour = true
